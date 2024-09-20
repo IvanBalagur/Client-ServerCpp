244a2e3eb349554f3dec0c6ec212e4cef346cdfc
IvanBalagur <ivan.artemov.97@mail.ru>
2024-09-20 17:23:31 +0700

    Добавьте файлы проекта.

---
+++ ServerCppTest.cpp
@@ -0,0 +1,31 @@
+﻿// ServerCppTest.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include ".vs/ServerCppTest/httplib.h"
+#include <fstream>
+#include <mutex>
+
+using namespace std;
+mutex mtx;
+
+void append_to_file(const string& content) 
+{
+    lock_guard<mutex> lock(mtx);
+    ofstream file("messages.txt", ios_base::app);
+    file << content << endl;
+}
+
+int main() 
+{
+    httplib::Server svr;
+
+    svr.Post("/message", [](const httplib::Request& req, httplib::Response& res) 
+        {
+        auto body = req.body;
+        append_to_file(body); // Записывает в файл запрос в формате "номер:сообщение:время H:М:S"  
+        });
+
+    svr.listen("localhost", 8080);
+
+    return 0;
+}
